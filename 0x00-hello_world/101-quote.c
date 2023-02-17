d1a07e854f98ebdae9d2e046ab74d7e5d3b6edc6
amostungu <amostungu1@gmail.com>
2023-02-16 16:21:05 -0800

    Task 8.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that print a line to the standard error using fput function
- * Return: 1 if success else 0 on error
+ * main - A program that print a line to a standard error using fput function
+ * Return: 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
