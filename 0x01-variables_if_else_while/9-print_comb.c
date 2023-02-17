82e0ebbccdb0aa1989c4ef480948b5fba52d0151
amostungu <amostungu1@gmail.com>
2023-02-17 15:34:04 -0800

    Task 9.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all combinations of single-digit numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
