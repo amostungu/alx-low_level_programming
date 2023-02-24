8d454685d74482bfe790972d084b08f3512a3333
amostungu <amostungu1@gmail.com>
2023-02-23 18:22:33 -0800

    Task 0.

---
+++ 0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - check upper letter.
+ *
+ * Return: 1 for uppercase else 0.
+ */
+int _isupper(int x)
+{
+	if (x >= 'A' && x <= 'Z')
+	{
+		ruturn (1)
+	}
+	return (0);
+}
