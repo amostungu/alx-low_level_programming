257d7c2b5bedd2a6060746634b98d63928609347
amostungu <amostungu1@gmail.com>
2023-02-27 11:43:56 -0800

    Task 4.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - Prints string in reverse
+ * @s: String to print
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\n')
+	{
+		c++;
+	}
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
