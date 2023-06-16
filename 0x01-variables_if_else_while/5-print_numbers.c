7f5f8c57a77050ecfd47b4a9f841b7d330150eee
amostungu <amostungu1@gmail.com>
2023-06-16 09:45:53 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program to print number less base 10
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
